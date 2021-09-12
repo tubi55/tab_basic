588b7ae003bcaf3c6c23a46decadc880cf9e5c86
tubi55 <tubi55@nate.com>
2021-09-12 14:04:14 +0900

    활성화 재클릭방지, 모션중 재클릭 방지

--- main.js
+++ main.js
@@ -1,25 +1,32 @@
 var $tab = $("#tab");
 var $btns = $tab.find(".btns li a");
 var $boxs = $tab.find(".boxs div");
-var speed = 500;
+var speed = 5000;
+var enableClick = true;
 
 $btns.on("click", function(e){
     e.preventDefault();
 
-    //클릭한 요소의 href속성을 구함
-    var target = $(this).attr("href");
-    console.log(target);
+    var isOn = $(this).hasClass("on");
+    if(isOn) return;
 
-    //순간적으로 모든 박스를 비활성화 시키고
-    $boxs.fadeOut(speed);
-    //target으로 받는 선택자만 활성화
-    $(target).fadeIn(speed);
-
-    
-    //순간적으로 모든 버튼 비활성화
-    $btns.removeClass("on");
-    //클릭한 요소만 활성화
-    $(this).addClass("on");
+    if(enableClick){
+        enableClick = false;
+        activation(this);
+    }    
 });
+
+function activation(el){
+    console.log("activaiton");
+    var target = $(el).attr("href");
+
+    $boxs.fadeOut(speed);
+    $(target).fadeIn(speed,function(){
+        enableClick = true;
+    });
+
+    $btns.removeClass("on");
+    $(el).addClass("on");
+}
 
 
